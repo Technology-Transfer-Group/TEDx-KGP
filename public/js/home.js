c4237c67cde17d6c3a554220bbe40e9e30337dcc
nagmus <ssumugan007@gmail.com>
2018-09-15 17:51:33 +0530

    remove date

--- public/js/home.js
+++ public/js/home.js
@@ -1,24 +1,4 @@
 var positions = []
-var countDownDate = new Date("Mar 10, 2018 16:30:00").getTime()
-
-var x = setInterval(function() {
-    var now = new Date().getTime()
-    var distance = countDownDate - now
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
-    $("#days").text(days)
-    $("#hours").text(hours)
-    $("#minutes").text(minutes)
-    $("#seconds").text(seconds)
-
-    if (distance < 0) {
-        clearInterval(x)
-        $("#timer").hide()
-    }
-}, 1000)
 
 var app = angular.module('app', [])
 
