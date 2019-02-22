f7c15bf1d0958a271cb7e8491b37592cc59d438a
nagmus <ssumugan007@gmail.com>
2019-02-22 16:05:30 +0530

    add notif

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,7 @@
+$(".notif").click(function() {
+  $(".notif").removeClass("notif");
+})
+
 var positions = []
 
 var app = angular.module('app', [])
@@ -112,23 +116,23 @@ window.onload = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("dayDisplay").textContent =  days;
   document.getElementById("minuteDisplay").textContent =  minutes;
   document.getElementById("hourDisplay").textContent =  hours;
   document.getElementById("secondDisplay").textContent =  seconds;
-    
-  // If the count down is over, write some text 
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
