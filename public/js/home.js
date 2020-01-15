8e360706ff2ebbffad86d16b3163f3ec12752e66
Shakul Pathak <prithupathak@gmail.com>
2020-01-15 14:54:47 +0530

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -109,7 +109,7 @@ scrollButtons[3].onclick = function(){
     involveDiv.scrollIntoView({behavior:"smooth", block:"start"});
 }
 // Set the date we're counting down to
-var countDownDate = new Date("March 16, 2019 10:37:25").getTime();
+var countDownDate = new Date("March 21, 2020 10:37:25").getTime();
 
 // Update the count down every 1 second
 window.onload = setInterval(function() {
