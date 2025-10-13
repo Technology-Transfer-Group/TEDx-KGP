b2d0d46842f22a80aa239dd3d9fbe7c6e1c0f19d
HariomArka <swapanghosh.8145@gmail.com>
2025-10-14 00:36:18 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', [])
 app.controller('teamController', function($scope, $http) {
-    $http.get('data/team.json').then(function(results) {
+    $http.get('data/teamcurr.json').then(function(results) {
         $scope.scoordiesteam = results.data.scoordies
         $scope.coordinatorsteam = results.data.coordinators
         $scope.headsteam = results.data.heads
